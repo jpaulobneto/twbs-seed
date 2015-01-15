9fbe70edf7bf68c5d82426b9244537d07da0247f
João Paulo <jpaulobneto@gmail.com>
2015-01-15 00:29:51 -0300

    Criando a task compass, testando-a e ajustando o primeiro clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var browserSync = require('browser-sync'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat'),
 	csscomb = require('gulp-csscomb'),
+	csso = require('gulp-csso'),
 	del = require('del'),
 	gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	imagemin = require('gulp-imagemin'),
-	nib = require('nib'),
+	plumber = require('gulp-plumber'),
 	reload = browserSync.reload,
 	runSequence = require('run-sequence'),
 	size = require('gulp-size'),
-	stylus = require('gulp-stylus'),
 	uglify = require('gulp-uglify'),
 	watch = require('gulp-watch');
 
@@ -19,23 +20,24 @@ var src = 'src',
 		js: src + '/assets/js',
 		css: src + '/assets/css',
 		img: src + '/assets/img',
-		styl: src + '/assets/stylus',
+		sass: src + '/assets/sass',
 		vend: src + '/assets/vendor'
 	},
 	build = {
 		js: dist + '/assets/js',
 		css: dist + '/assets/css',
 		img: dist + '/assets/img',
-		styl: dist + '/assets/stylus',
+		sass: dist + '/assets/sass',
 		vend: dist + '/assets/vendor'
 	};
 
 // 'full' or 'assets'
 var distMode = 'full',
-	minJs = true;
+	minCss = false,
+	minJs = false;
 
 // browser-sync task for starting the server.
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
 	browserSync({
 		server: {
 			baseDir: src
@@ -45,14 +47,14 @@ gulp.task('browser-sync', function() {
 });
 
 // Reload all Browsers
-gulp.task('browser-reload', function() {
+gulp.task('browser-reload', function () {
 	browserSync.reload();
 });
 
-gulp.task('build', function(cb) {
+gulp.task('build', function (cb) {
 	runSequence(
-		'cleanByDistMode',
-		['stylus', 'stylus:ie', 'concat'],
+		'cleanDist',
+		['compass', 'concat'],
 		'copyByDistMode',
 		// 'csscomb:dist',
 		['clean:sprite', 'uglify'],
@@ -63,60 +65,74 @@ gulp.task('build', function(cb) {
 	);
 });
 
-gulp.task('clean:release', function(cb) {
+gulp.task('clean:release', function (cb) {
 	return del([
-		build.styl,
+		build.sass,
 		build.js + '/**/*',
 		'!' + build.js + '/scripts.js',
 		build.vend
 	], cb);
 });
 
-gulp.task('clean:sprite', function(cb) {
+gulp.task('clean:sprite', function (cb) {
 	return del([
 		build.img + '/sprite/hd',
 		build.img + '/sprite/standard'
 	], cb);
 });
 
-gulp.task('cleanByDistMode', function(cb) {
-	switch (distMode) {
-		case 'assets':
-			return del(dist + '/assets', cb);
-			break;
-		default:
-			return del(dist, cb);
+gulp.task('cleanDist', function (cb) {
+	return del(dist, cb);
+});
+
+gulp.task('compass', function () {
+	var config = {
+		css: dirs.css,
+		sass: dirs.sass,
+		image: dirs.img,
+		javascript: dirs.js
+	};
+
+	if (minCss) {
+		return gulp.src([
+			dirs.sass + '/main.scss',
+			//dirs.sass + '/main-ie.scss',
+		])
+			.pipe(plumber())
+			.pipe(compass(config))
+			.pipe(csso())
+			.pipe(gulp.dest(dirs.css))
+			.pipe(reload({stream: true}));
+	} else {
+		return gulp.src([
+			dirs.sass + '/main.scss',
+			//dirs.sass + '/main-ie.scss',
+		])
+			.pipe(plumber())
+			.pipe(compass(config))
+			.pipe(gulp.dest(dirs.css))
+			.pipe(reload({stream: true}));
 	}
 });
 
-gulp.task('concat', function() {
+gulp.task('concat', function () {
 	return gulp.src([
 		// Plugins
 		dirs.js + '/plugins.js',
 		// Vendors
-		dirs.vend + '/bootstrap-stylus/js/transition.js',
-		dirs.vend + '/bootstrap-stylus/js/alert.js',
-		dirs.vend + '/bootstrap-stylus/js/button.js',
-		dirs.vend + '/bootstrap-stylus/js/carousel.js',
-		dirs.vend + '/bootstrap-stylus/js/collapse.js',
-		dirs.vend + '/bootstrap-stylus/js/dropdown.js',
-		dirs.vend + '/bootstrap-stylus/js/modal.js',
-		dirs.vend + '/bootstrap-stylus/js/tooltip.js',
-		dirs.vend + '/bootstrap-stylus/js/popover.js',
-		dirs.vend + '/bootstrap-stylus/js/scrollspy.js',
-		dirs.vend + '/bootstrap-stylus/js/tab.js',
-		dirs.vend + '/bootstrap-stylus/js/affix.js',
+		dirs.vend + '/bootstrap-sass-official/assets/javascripts/bootstrap.js',
 		// Main
 		dirs.js + '/main.js',
 		// Modules
 		dirs.js + '/modules/**/*.js'
 	])
+		.pipe(plumber())
 		.pipe(concat('scripts.js'))
 		.pipe(gulp.dest(dirs.js))
 		.pipe(reload({stream: true}));
 });
 
-gulp.task('copyByDistMode', function() {
+gulp.task('copyByDistMode', function () {
 	switch (distMode) {
 		case 'assets':
 			return gulp.src([
@@ -132,7 +148,7 @@ gulp.task('copyByDistMode', function() {
 	}
 });
 
-gulp.task('copy:vendors', function() {
+gulp.task('copy:vendors', function () {
 	return gulp.src([
 		dirs.vend + '/**/*jquery.min.js',
 		dirs.vend + '/**/*jquery.mask.min.js',
@@ -141,7 +157,7 @@ gulp.task('copy:vendors', function() {
 		.pipe(gulp.dest(build.vend));
 });
 
-gulp.task('csscomb:dist', function() {
+gulp.task('csscomb:dist', function () {
 	return gulp.src([
 		build.css + '/main.css',
 		build.css + '/main-ie.css'
@@ -150,8 +166,8 @@ gulp.task('csscomb:dist', function() {
 		.pipe(gulp.dest(build.css));
 });
 
-gulp.task('default', ['browser-sync'], function() {
-	gulp.watch(dirs.styl + '/**/*.styl', ['stylus']);
+gulp.task('default', ['browser-sync'], function () {
+	gulp.watch(dirs.sass + '/**/*.scss', ['compass']);
 	gulp.watch([
 		dirs.js + '/plugins.js',
 		dirs.js + '/main.js',
@@ -170,29 +186,7 @@ gulp.task('imagemin', function () {
 		.pipe(size({title: 'imagemin', showFiles: true}));
 });
 
-gulp.task('stylus', function() {
-	return gulp.src(dirs.styl + '/main.styl')
-		.pipe(stylus({
-			use: nib(),
-			compress: true,
-			linenos: false
-		}))
-		.pipe(gulp.dest(dirs.css))
-		.pipe(reload({stream: true}));;
-});
-
-gulp.task('stylus:ie', function() {
-	return gulp.src(dirs.styl + '/main-ie.styl')
-		.pipe(stylus({
-			use: nib(),
-			compress: true,
-			linenos: false
-		}))
-		.pipe(gulp.dest(dirs.css))
-		.pipe(reload({stream: true}));;
-});
-
-gulp.task('uglify', function() {
+gulp.task('uglify', function () {
 	if (minJs) {
 		return gulp.src(build.js + '/scripts.js')
 			.pipe(uglify().on('error', gutil.log))

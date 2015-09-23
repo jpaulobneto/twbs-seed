880107607edad6ae03817fd688eb62353fc1066b
João Paulo <jpaulobneto@gmail.com>
2015-09-23 17:29:49 -0300

    Reidentando o código

--- src/assets/js/modules/common.js
+++ src/assets/js/modules/common.js
@@ -1,15 +1,15 @@
 // Common
 window.mod.common = function() {
 
-	// Scope
-	var that = this;
+  // Scope
+  var that = this;
 
-	var init = function() {
-		console.log('[my] begin common.js');
-		window.mod['maps'] = new window.mod['maps']();
-		window.mod['mask'] = new window.mod['mask']();
-	};
+  var init = function() {
+    console.log('[my] begin common.js');
+    window.mod['maps'] = new window.mod['maps']();
+    window.mod['mask'] = new window.mod['mask']();
+  };
 
-	init();
+  init();
 
 };
--- src/assets/js/modules/jquery.gmaps.js
+++ src/assets/js/modules/jquery.gmaps.js
@@ -1,58 +1,58 @@
-(function ($) {
+(function($) {
 
-	$.fn.gmaps = function(options) {
+  $.fn.gmaps = function(options) {
 
-	return this.each(function() {
-		var settings = $.extend({
-			zoom: 16,
-			scrollwheel: false,
-			center: new google.maps.LatLng(-7.119764,-34.872458)
-		},options);
-		var map = new google.maps.Map(this,settings);
+    return this.each(function() {
+      var settings = $.extend({
+        zoom: 16,
+        scrollwheel: false,
+        center: new google.maps.LatLng(-7.119764, -34.872458)
+      }, options);
+      var map = new google.maps.Map(this, settings);
 
-		var markerOptions = {};
+      var markerOptions = {};
 
-		if($(this).data('icon-size') !== undefined && $(this).data('icon') !== undefined){
-			var sizes = $(this).data('icon-size').split(',');
-			var size1 = parseInt(sizes[0]);
-			var size2 = parseInt(sizes[1]);
-			markerOptions['icon'] = new google.maps.MarkerImage($(this).data('icon'), null, null, null, new google.maps.Size(size1,size2));
-		}else if($(this).data('icon') !== undefined){
-			markerOptions['icon'] = $(this).data('icon');
-		}
+      if ($(this).data('icon-size') !== undefined && $(this).data('icon') !== undefined) {
+        var sizes = $(this).data('icon-size').split(',');
+        var size1 = parseInt(sizes[0]);
+        var size2 = parseInt(sizes[1]);
+        markerOptions['icon'] = new google.maps.MarkerImage($(this).data('icon'), null, null, null, new google.maps.Size(size1, size2));
+      } else if ($(this).data('icon') !== undefined) {
+        markerOptions['icon'] = $(this).data('icon');
+      }
 
-		if($(this).data('latitude') !== undefined && $(this).data('longitude') !== undefined){
-			markerOptions['position'] = new google.maps.LatLng(parseFloat($(this).data('latitude')), parseFloat($(this).data('longitude')));
-		}
+      if ($(this).data('latitude') !== undefined && $(this).data('longitude') !== undefined) {
+        markerOptions['position'] = new google.maps.LatLng(parseFloat($(this).data('latitude')), parseFloat($(this).data('longitude')));
+      }
 
-		if($(this).data('title') !== undefined && $(this).data('text') !== undefined){
-			markerOptions['content'] = [
-				'<b>',
-				$(this).data('title'),
-				'</b> <br / >',
-				$(this).data('text')
+      if ($(this).data('title') !== undefined && $(this).data('text') !== undefined) {
+        markerOptions['content'] = [
+          '<b>',
+          $(this).data('title'),
+          '</b> <br / >',
+          $(this).data('text')
 
-			].join('');
-		}
+        ].join('');
+      }
 
-		var markerSettings = $.extend({
-			map: map
-		}, markerOptions);
+      var markerSettings = $.extend({
+        map: map
+      }, markerOptions);
 
-		var marker = new google.maps.Marker(markerSettings);
-		console.log(marker);
-		map.setCenter(markerOptions['position']);
+      var marker = new google.maps.Marker(markerSettings);
+      console.log(marker);
+      map.setCenter(markerOptions['position']);
 
-		var infowindow = new google.maps.InfoWindow({
-			content: markerSettings['content']
-		});
+      var infowindow = new google.maps.InfoWindow({
+        content: markerSettings['content']
+      });
 
-		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.open(map,marker);
-			map.panTo(marker.getPosition());
-		});
-	});
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+        map.panTo(marker.getPosition());
+      });
+    });
 
-};
+  };
 
-}( jQuery ));
+}(jQuery));
